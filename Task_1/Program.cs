9cc34789742863a7424c7c4640022dfea5b05db1
Irina <Aniri_Varga@mail.ru>
2022-09-03 18:04:15 +0300

    added task on square numbers

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,1 +1,7 @@
-﻿
+﻿// Задача 1. Напишите программу, которая на входе принимает число и на выходе выводит его квадрат
+Console.Clear();
+Console.Write("Enter an integer: ");
+int num = Convert.ToInt32(Console.ReadLine()); // int num = int.Parse(Console.ReadLine()) - та же команда, но не учитывает нулевое значение
+int square = num * num;
+Console.WriteLine($"Square number of {num} is {square}"); // возможно Console.WriteLine("Square number of" + num + "is" + square")
+
