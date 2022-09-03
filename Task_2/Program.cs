7eef3cc711a062333957c87f10a747069889f3ae
Irina <Aniri_Varga@mail.ru>
2022-09-03 18:52:13 +0300

    added the task about checking on square

---
+++ Task_2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на входе принимает два числа и определяет является ли первое квадратом другого.
+Console.Clear();
+Console.Write("Enter the first digit: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the second digit: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+int square = num2 * num2;
+if (square == num1)
+{
+    Console.WriteLine($"Digit {num1} is the square to {num2}");
+}
+else
+{
+    Console.WriteLine($"Digit {num1} is not the square to {num2}");
+}
