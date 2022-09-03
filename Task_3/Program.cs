f0163da96a2598aa247316c1264d4132b76de9bf
Irina <Aniri_Varga@mail.ru>
2022-09-03 19:23:25 +0300

    added comment on syntaxis in task 3

--- Task_3/Program.cs
+++ Task_3/Program.cs
@@ -2,27 +2,48 @@
 Console.Clear();
 Console.WriteLine("Write the number from 1 to 7: ");
 string day = Console.ReadLine();
-if (day == "1") {
+if (day == "1")
+{
     Console.WriteLine("Monday");
 }
-else if (day == "2") {
+else if (day == "2")
+{
     Console.WriteLine("Tuesday");
 }
-else if (day == "3") {
+else if (day == "3")
+{
     Console.WriteLine("Wednesday");
 }
-else if (day == "4") {
+else if (day == "4")
+{
     Console.WriteLine("Thursday");
 }
-else if (day == "5") {
+else if (day == "5")
+{
     Console.WriteLine("Friday");
 }
-else if (day == "6") {
+else if (day == "6")
+{
     Console.WriteLine("Saturday");
 }
-else if (day == "7") {
+else if (day == "7")
+{
     Console.WriteLine("Sunday");
 }
-else {
+else
+{
     Console.WriteLine("There is no day under this number, sorry :(");
 }
+
+// Если после оператора условия стоит только одно требование, то фигурные скобки можно опустить // Напишите программу, по которой пользователь вводит номер дня недели, а программа пишет его название
+// Console.Clear();
+// Console.WriteLine("Write the number from 1 to 7: ");
+// string day = Console.ReadLine();
+// if (day == "1") Console.WriteLine("Monday");
+// else if (day == "2") Console.WriteLine("Tuesday");
+// else if (day == "3") Console.WriteLine("Wednesday");
+// else if (day == "4") Console.WriteLine("Thursday");
+// else if (day == "5") Console.WriteLine("Friday");
+// else if (day == "6") Console.WriteLine("Saturday");
+// else if (day == "7") Console.WriteLine("Sunday");
+// else Console.WriteLine("There is no day under this number, sorry :(");
