b19a8ffe44341b1a3743bf0b15c81ff918870d00
Irina <Aniri_Varga@mail.ru>
2022-09-04 19:48:39 +0300

    Corrected the font

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -3,5 +3,6 @@ Console.Clear();
 Console.Write("Enter an integer: ");
 int num = Convert.ToInt32(Console.ReadLine()); // int num = int.Parse(Console.ReadLine()) - та же команда, но не учитывает нулевое значение
 int square = num * num;
-Console.WriteLine($"Square number of {num} is {square}"); // возможно Console.WriteLine("Square number of" + num + "is" + square")
+Console.WriteLine($"Square number of {num} is {square}"); 
+// возможно Console.WriteLine("Square number of" + num + "is" + square")
 
