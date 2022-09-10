83cedfb204464a997d42c668fc519e562d3acd22
Irina <Aniri_Varga@mail.ru>
2022-09-10 17:23:46 +0300

    added task 7

---
+++ Task_7/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая выдает случайное трехзначное число и 
+// удаляет вторую цифру этого числа. 
+// Например: 751 ->71, 483->43
+
+int number = new Random().Next(100, 1000); //Определяем случайное число
+
+int secondOut(int num) // Задаём метод
+{
+    int fristDigit = num / 100 * 10;
+    int secondDigit = num % 10;
+    return fristDigit + secondDigit;
+}
+int neededNum = secondOut(number); //Помещаем результат метода в переменную neededNum
+Console.WriteLine($"{number} -> {neededNum}"); //Выводим результат в консоль
