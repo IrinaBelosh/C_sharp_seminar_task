7f0fff9ab0b17390ed1f9b35ac068bdc3f95afdb
Irina <Aniri_Varga@mail.ru>
2022-09-16 20:16:55 +0300

    added task 16

---
+++ Task_16/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 26. напишите программу, которая принимает на вход число и выдает сколько цифр в числе
+// например
+// 7 -> 1
+// 12345 -> 5
+// 525 -> 3
+
+Console.Clear();
+Console.Write("Enter a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int DigitsNumber(int num)
+{
+    if (num != 0)
+    {
+        if (num < 0) num = -num;
+        int i = default;
+        while (num > 0)
+        {
+            num = num / 10;
+            i = i + 1;
+        }
+        return i;
+    }
+    return 1;
+}
+int digits = DigitsNumber(number);
+Console.WriteLine($"{number} -> {digits}");
+
