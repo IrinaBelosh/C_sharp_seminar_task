04ae28fd473ceb735490102b7eac128cfd3c4390
Irina <Aniri_Varga@mail.ru>
2022-09-27 17:11:23 +0300

    added task 31

---
+++ Task_31/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 49: Задайте двумерный массив. Найдите элементы, у
+// которых оба индекса чётные, и замените эти элементы на их
+// квадраты.
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    Random rnd = new Random();
+    int[,] matr = new int[rows, columns];
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matr;
+}
+
+void ChangeEvenNumbersToSquare(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (i % 2 == 0 && j % 2 == 0) matrix[i, j] = matrix[i, j] * matrix[i, j];
+        }
+    }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+        for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j<matrix.GetLength(1)-1) Console.Write($"{matrix[i,j], 3}, ");
+            else Console.Write($"{matrix[i,j],3} ");
+        }
+        Console.WriteLine("]");
+    }
+}
+
+int[,] matrix = CreateMatrixRndInt(6,7,1,9);
+PrintMatrix(matrix);
+ChangeEvenNumbersToSquare(matrix);
+Console.WriteLine();
+PrintMatrix(matrix);
